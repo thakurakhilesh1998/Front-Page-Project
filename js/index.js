e2a631be957b2e8ab378d39a1974515af0cc4e10
thakurakhilesh1998 <39956027+thakurakhilesh1998@users.noreply.github.com>
2020-11-30 13:24:42 +0530

    third

---
+++ js/index.js
@@ -0,0 +1,19 @@
+var messages=['Secure','Simplicity','Adapted'];
+var rank=0;
+var element=document.getElementById("type-msg");
+function show()
+{
+    text();
+    (rank==messages.length-1)?rank=0:rank++;
+    setTimeout(show,1000);
+}
+
+function text(j) {
+    var j=0;
+    if(j<messages.length)
+    {
+        element.innerHTML=messages[rank].charAt(j);
+        j++;
+        setTimeout(text=>(j),500);
+    }
+  }
